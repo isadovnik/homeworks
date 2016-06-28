bd67dfb75567102a6e6d57ae2c938b1a286334cd
Vladislav Zayko <vladislav.zayko@racingpost.com>
2016-06-28 18:07:12 +0300

    Task 10 add conditionals

--- task10.php
+++ task10.php
@@ -2,11 +2,17 @@
 $text = "aaaa:wwwwww:iiiiiii";
 
 echo $pos = strpos($text, ':');
-echo substr($text, 0 , $pos+1);
+if($pos !== false) {
+    echo substr($text, 0, $pos + 1);
+}
 
 echo "<br>";
 echo $text2 = substr($text, $pos+1);
 
 echo "<br>";
 $pos2 = strpos($text2, ':');
-echo substr($text2, 0, $pos2);
+if($pos2 !== false ) {
+    echo substr($text2, 0, $pos2);
+} else {
+    echo $text2;
+}
